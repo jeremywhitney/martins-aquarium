1c0098ebf51f33193de9a53152eae4b95885fd69
Jeremy Whitney <bearwhitness@gmail.com>
2024-11-18 11:43:02 -0600

    Created interfaces in types.ts, and refactored fishData to use them

--- scripts/fishData.ts
+++ scripts/fishData.ts
@@ -1,158 +1,162 @@
-export const database = {
-    fish: [
-        {
-            name: "Nemo",
-            species: "Clownfish",
-            length: 3,
-            location: "Great Barrier Reef",
-            diet: "Omnivore",
-            image: "./images/nemo.webp"
-        },
-        {
-            name: "Dory",
-            species: "Blue Tang",
-            length: 5,
-            location: "Great Barrier Reef",
-            diet: "Omnivore",
-            image: "./images/dory.webp"
-        },
-        {
-            name: "Gill",
-            species: "Moorish Idol",
-            length: 8,
-            location: "Indian & Pacific Oceans",
-            diet: "Omnivore",
-            image: "./images/gill.webp"
-        },
-        {
-            name: "Bloat",
-            species: "Porcupine pufferfish",
-            length: 20,
-            location: "Great Barrier Reef",
-            diet: "Omnivore",
-            image: "./images/bloat.webp"
-        },
-        {
-            name: "Sheldon",
-            species: "Seahorse",
-            length: 6,
-            location: "Great Barrier Reef",
-            diet: "Crustaceans",
-            image: "./images/sheldon.webp"
-        },
-        {
-            name: "Bubbles",
-            species: "Yellow Tang",
-            length: 8,
-            location: "Great Barrier Reef",
-            diet: "Omnivore",
-            image: "./images/bubbles.webp"
-        },
-        {
-            name: "Anchor",
-            species: "Hammerhead Shark",
-            length: 144,
-            location: "Worldwide",
-            diet: "Carnivore",
-            image: "./images/anchor.webp"
-        },
-        {
-            name: "Marlin",
-            species: "Striped Marlin",
-            length: 120,
-            location: "Pacific Ocean",
-            diet: "Carnivore",
-            image: "./images/marlin.jpeg"
-        },
-        {
-            name: "Dolphin",
-            species: "Common Dolphin",
-            length: 84,
-            location: "Worldwide",
-            diet: "Carnivore",
-            image: "./images/dolphin.jpeg"
-        },
-        {
-            name: "Squirt",
-            species: "Squid",
-            length: 10,
-            location: "Ocean",
-            diet: "Carnivore",
-            image: "./images/squirt.jpeg"
-        },
-        {
-            name: "Pearl",
-            species: "Pearl Gourami",
-            length: 4,
-            location: "Southeast Asia",
-            diet: "Omnivore",
-            image: "./images/pearl.webp"
-        },
-        {
-            name: "Clowny",
-            species: "Clown Triggerfish",
-            length: 18,
-            location: "Indian Ocean",
-            diet: "Carnivore",
-            image: "./images/clowny.png"
-        },
-        {
-            name: "Barracuda",
-            species: "Great Barracuda",
-            length: 72,
-            location: "Tropical Seas",
-            diet: "Carnivore",
-            image: "./images/barracuda.jpeg"
-        },
-        {
-            name: "Jelly",
-            species: "Jellyfish",
-            length: 15,
-            location: "Worldwide",
-            diet: "Carnivore",
-            image: "./images/jelly.jpeg"
-        },
-        {
-            name: "Guppy",
-            species: "Millionfish",
-            length: 2,
-            location: "South America",
-            diet: "Omnivore",
-            image: "./images/guppy.jpeg"
-        },
-        {
-            name: "Esteban",
-            species: "Jaguar Shark",
-            length: 180,
-            location: "Unknown",
-            diet: "Carnivore",
-            image: "./images/esteban.webp"
-        },
-        {
-            name: "Angelfish",
-            species: "French Angelfish",
-            length: 15,
-            location: "Caribbean Sea",
-            diet: "Omnivore",
-            image: "./images/angelfish.webp"
-        }
-        
-    ],
-    tips: [
-        {
-            topic: "Water Temperature",
-            text: "Maintain a consistent water temperature between 75-80°F (24-27°C) for most tropical fish."
-        },
-        {
-            topic: "Feeding Guidelines",
-            text: "Feed omnivores four times a day and carnivores twice a day."
-        }
-    ],
-    locations: [
-        {
-            name: "Great Barrier Reef",
-            country: "Australia",
-            description: "The Great Barrier Reef is the world's largest coral reef system, supporting a diverse array of marine life."
-        }
-    ]
-}
+import { Fish, Location, Tip } from "./types";
+
+const fish: Fish[] = [
+  {
+    name: "Nemo",
+    species: "Clownfish",
+    length: 3,
+    location: "Great Barrier Reef",
+    diet: "Omnivore",
+    image: "./images/nemo.webp",
+  },
+  {
+    name: "Dory",
+    species: "Blue Tang",
+    length: 5,
+    location: "Great Barrier Reef",
+    diet: "Omnivore",
+    image: "./images/dory.webp",
+  },
+  {
+    name: "Gill",
+    species: "Moorish Idol",
+    length: 8,
+    location: "Indian & Pacific Oceans",
+    diet: "Omnivore",
+    image: "./images/gill.webp",
+  },
+  {
+    name: "Bloat",
+    species: "Porcupine pufferfish",
+    length: 20,
+    location: "Great Barrier Reef",
+    diet: "Omnivore",
+    image: "./images/bloat.webp",
+  },
+  {
+    name: "Sheldon",
+    species: "Seahorse",
+    length: 6,
+    location: "Great Barrier Reef",
+    diet: "Crustaceans",
+    image: "./images/sheldon.webp",
+  },
+  {
+    name: "Bubbles",
+    species: "Yellow Tang",
+    length: 8,
+    location: "Great Barrier Reef",
+    diet: "Omnivore",
+    image: "./images/bubbles.webp",
+  },
+  {
+    name: "Anchor",
+    species: "Hammerhead Shark",
+    length: 144,
+    location: "Worldwide",
+    diet: "Carnivore",
+    image: "./images/anchor.webp",
+  },
+  {
+    name: "Marlin",
+    species: "Striped Marlin",
+    length: 120,
+    location: "Pacific Ocean",
+    diet: "Carnivore",
+    image: "./images/marlin.jpeg",
+  },
+  {
+    name: "Dolphin",
+    species: "Common Dolphin",
+    length: 84,
+    location: "Worldwide",
+    diet: "Carnivore",
+    image: "./images/dolphin.jpeg",
+  },
+  {
+    name: "Squirt",
+    species: "Squid",
+    length: 10,
+    location: "Ocean",
+    diet: "Carnivore",
+    image: "./images/squirt.jpeg",
+  },
+  {
+    name: "Pearl",
+    species: "Pearl Gourami",
+    length: 4,
+    location: "Southeast Asia",
+    diet: "Omnivore",
+    image: "./images/pearl.webp",
+  },
+  {
+    name: "Clowny",
+    species: "Clown Triggerfish",
+    length: 18,
+    location: "Indian Ocean",
+    diet: "Carnivore",
+    image: "./images/clowny.png",
+  },
+  {
+    name: "Barracuda",
+    species: "Great Barracuda",
+    length: 72,
+    location: "Tropical Seas",
+    diet: "Carnivore",
+    image: "./images/barracuda.jpeg",
+  },
+  {
+    name: "Jelly",
+    species: "Jellyfish",
+    length: 15,
+    location: "Worldwide",
+    diet: "Carnivore",
+    image: "./images/jelly.jpeg",
+  },
+  {
+    name: "Guppy",
+    species: "Millionfish",
+    length: 2,
+    location: "South America",
+    diet: "Omnivore",
+    image: "./images/guppy.jpeg",
+  },
+  {
+    name: "Esteban",
+    species: "Jaguar Shark",
+    length: 180,
+    location: "Unknown",
+    diet: "Carnivore",
+    image: "./images/esteban.webp",
+  },
+  {
+    name: "Angelfish",
+    species: "French Angelfish",
+    length: 15,
+    location: "Caribbean Sea",
+    diet: "Omnivore",
+    image: "./images/angelfish.webp",
+  },
+];
+
+const tips: Tip[] = [
+  {
+    topic: "Water Temperature",
+    text: "Maintain a consistent water temperature between 75-80°F (24-27°C) for most tropical fish.",
+  },
+  {
+    topic: "Feeding Guidelines",
+    text: "Feed omnivores four times a day and carnivores twice a day.",
+  },
+];
+
+const locations: Location[] = [
+  {
+    name: "Great Barrier Reef",
+    country: "Australia",
+    description:
+      "The Great Barrier Reef is the world's largest coral reef system, supporting a diverse array of marine life.",
+  },
+];
+
+export const database = { fish, tips, locations };
---
+++ scripts/types.ts
@@ -0,0 +1,19 @@
+export interface Fish {
+  name: string;
+  species: string;
+  length: number;
+  location: string;
+  diet: string;
+  image: string;
+}
+
+export interface Tip {
+  topic: string;
+  text: string;
+}
+
+export interface Location {
+  name: string;
+  country: string;
+  description: string;
+}
